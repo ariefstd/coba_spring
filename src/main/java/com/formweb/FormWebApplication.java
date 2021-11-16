888b4594b0d45439cafaa1199c34ddeeb11329fc
ariefstd <arief.setiyadi@yahoo.com>
2021-11-16 21:23:40 +0700

    first commit

---
+++ src/main/java/com/formweb/FormWebApplication.java
@@ -0,0 +1,13 @@
+package com.formweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FormWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FormWebApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/formweb/FormWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.formweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FormWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
